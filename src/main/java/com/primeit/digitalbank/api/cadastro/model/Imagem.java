fa276e36a6718fdb9077d7ee98ad3cfd4d0f2eb1
Thales Navasques <thales.navasques@gmail.com>
2022-08-08 13:19:49 -0300

    Criação dos Métodos da Classe Model Imagem

--- src/main/java/com/primeit/digitalbank/api/cadastro/model/Imagem.java
+++ src/main/java/com/primeit/digitalbank/api/cadastro/model/Imagem.java
@@ -3,6 +3,9 @@ package com.primeit.digitalbank.api.cadastro.model;
 import java.sql.Timestamp;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +29,56 @@ public class Imagem {
 		this.tipo = tipo;
 		this.frente = frente;
 		this.verso = verso;
+		this.dtCriacao = dtCriacao;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getIdImg() {
+		return idImg;
+	}
+
+	public void setIdImg(int idImg) {
+		this.idImg = idImg;
+	}
+
+	public int getIdCad() {
+		return idCad;
+	}
+
+	public void setIdCad(int idCad) {
+		this.idCad = idCad;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public boolean isFrente() {
+		return frente;
+	}
+
+	public void setFrente(boolean frente) {
+		this.frente = frente;
+	}
+
+	public boolean isVerso() {
+		return verso;
+	}
+
+	public void setVerso(boolean verso) {
+		this.verso = verso;
+	}
+
+	public Timestamp getDtCriacao() {
+		return dtCriacao;
+	}
+
+	public void setDtCriacao(Timestamp dtCriacao) {
 		this.dtCriacao = dtCriacao;
 	}
 	
