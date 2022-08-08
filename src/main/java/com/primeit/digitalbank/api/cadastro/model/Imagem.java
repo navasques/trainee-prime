074e3dc09cf3393c33b48c76b7d35f2f16b36d39
Thales Navasques <thales.navasques@gmail.com>
2022-08-08 13:14:47 -0300

    Criação da Classe Model Imagens

---
+++ src/main/java/com/primeit/digitalbank/api/cadastro/model/Imagem.java
@@ -0,0 +1,19 @@
+package com.primeit.digitalbank.api.cadastro.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cadastro")
+public class Imagem {
+
+	private int idImg;
+	private int idCad;
+	private String tipo;
+	private boolean frente; 
+	private boolean verso;
+	private Timestamp dtCriacao;
+	
+}
