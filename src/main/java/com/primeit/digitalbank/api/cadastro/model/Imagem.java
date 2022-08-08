609af97e33d6788463650039d58dbf628ddd6cac
Thales Navasques <thales.navasques@gmail.com>
2022-08-08 11:44:09 -0300

    Criação da classe Model Imagem

---
+++ src/main/java/com/primeit/digitalbank/api/cadastro/model/Imagem.java
@@ -0,0 +1,10 @@
+package com.primeit.digitalbank.api.cadastro.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "cadastro")
+public class Imagem {
+
+}
