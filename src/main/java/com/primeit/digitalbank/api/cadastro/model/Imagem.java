52f841468ed11615002b03bc8b0c0987defd6b33
Thales Navasques <thales.navasques@gmail.com>
2022-08-08 12:23:28 -0300

    Cração das variáveis

--- src/main/java/com/primeit/digitalbank/api/cadastro/model/Imagem.java
+++ src/main/java/com/primeit/digitalbank/api/cadastro/model/Imagem.java
@@ -1,10 +1,21 @@
 package com.primeit.digitalbank.api.cadastro.model;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name= "cadastro")
 public class Imagem {
-
+	
+	private int idImg;
+	private int idCad;
+	private String tipo;
+	private boolean frente;
+	private boolean verso;
+	private Timestamp dtCriacao;
+	
+	
+	
 }
